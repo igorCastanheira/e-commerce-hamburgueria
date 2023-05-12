e5e59eed571ab233f4bed67620fd206b35755cb6
igorCastanheira <igor.castanheira.dev@gmail.com>
2023-05-11 21:46:06 -0300

    Primeira entrega projeto

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,37 +2,40 @@ import React, { useEffect, useState } from "react";
 import "./styles/ItemListContainer.css"
 import ItemList from "./ItemList";
 import {products} from "../mock/products";
+import { useParams } from "react-router-dom";
 
 
 
 
 
 const ItemListContainer= ()=>{
-const [product,setProduct]= useState([]);
-
-
+//const [product,setProduct]= useState([]);
+const {categoryId} = useParams();
+const[filteredProducts,setFilteredProducts]=useState([]);
   const productList=()=>{
     return new Promise((resolve,reject)=>{
         resolve(products);
     })
   }
+console.log("categoryid"+categoryId);
+  useEffect(()=> {
 
-  useEffect(
-  ()=> {
-
-setTimeout(()=>{
- productList().then(result => setProduct(result));
-},2000)
+ productList()
+ .then((result)=>{
+  const produtos =!!categoryId ? result.filter(p=>p.itemCategory === categoryId):result
+ setFilteredProducts(produtos)})
 
   },
 
-  []);
+  [categoryId]);
+ 
 return(
     
     <div className="inner">
       <div>
       <div class="row row-cols-1 row-cols-md-2 g-4">
-    <ItemList items = {product}/>
+    
+    <ItemList items = {filteredProducts}/>
       </div>
       </div>
     </div>
