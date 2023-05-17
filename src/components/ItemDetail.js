cd5ed54b91f90c9f9c08e28eb90084bd6b4bea4f
igorCastanheira <igor.castanheira.dev@gmail.com>
2023-05-16 21:15:17 -0300

    adicionado evendo onclick para alterar valor

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,15 +1,24 @@
 import React from "react";
+import ItemCount from "./ItemCount";
+
+
+
+
 
 
 
 function ItemDetail ({item}){
-    
+ 
         return (
           <div key={item.id}>
             <h2>Id: {item.id}</h2>
             <h2>Nome: {item.name}</h2>
             <h2>Descrição:{item.description}</h2>
             <h2>Estoque: {item.stok}</h2>
+           <ItemCount/>
+  
+            
+          
           </div>
         );
       
